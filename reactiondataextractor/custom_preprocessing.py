cb65d0bb942aac6212680b3ae02f0ced392183e3
Cordelia Cavill <159061167+cmc227@users.noreply.github.com>
2025-05-28 13:07:15 +0100

    Update custom_preprocessing.py

--- reactiondataextractor/custom_preprocessing.py
+++ reactiondataextractor/custom_preprocessing.py
@@ -1,1 +1,23 @@
+from processors import ImageReader, ImageScaler, ImageNormaliser, Binariser
 
+def preprocess_for_arrows(image_path: str):
+    fig = ImageReader(image_path, color_mode=ImageReader.COLOR_MODE.GRAY).process()
+    fig = ImageScaler(fig, resize_min_dim_to=1024).process()
+    fig = ImageNormaliser(fig).process()
+    fig = Binariser(fig).process()
+    return fig
+
+def preprocess_for_diagrams(image_path: str):
+    fig = ImageReader(image_path, color_mode=ImageReader.COLOR_MODE.RGB).process()
+    fig = ImageScaler(fig, resize_min_dim_to=2048).process()
+    fig = ImageNormaliser(fig).process()
+    return fig
+
+def preprocess_for_labels(image_path: str):
+    fig = ImageReader(image_path, color_mode=ImageReader.COLOR_MODE.GRAY).process()
+    return fig
+
+def preprocess_for_conditions(image_path: str):
+    fig = ImageReader(image_path, color_mode=ImageReader.COLOR_MODE.GRAY).process()
+    fig = ImageScaler(fig, resize_min_dim_to=1024).process()
+    return fig
