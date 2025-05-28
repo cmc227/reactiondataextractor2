fe56ddf5542ac322737c30884877cd4b482095b2
Cordelia Cavill <159061167+cmc227@users.noreply.github.com>
2025-05-28 13:49:45 +0100

    Update custom_preprocessing.py

--- reactiondataextractor/custom_preprocessing.py
+++ reactiondataextractor/custom_preprocessing.py
@@ -50,5 +50,7 @@ def preprocess_for_conditions(image_path: str):
     print(f"[Preprocessing] Conditions: Loading and processing image {image_path}")
     fig = ImageReader(image_path, color_mode=ImageReader.COLOR_MODE.GRAY).process()
     fig = ImageScaler(fig, resize_min_dim_to=1024).process()
+    fig = ImageNormaliser(fig).process()
+    fig = Binariser(fig).process()
     print("[Preprocessing] Conditions: Done")
     return fig
